b4b187e6f491f441ce7d812fd96af3a69ccd6e5f
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-19 21:20:13 +0900

    Update: productId 추가 && 오타 수정

--- src/db/models/schemas/userSchema.js
+++ src/db/models/schemas/userSchema.js
@@ -1,10 +1,11 @@
-const { Schema } = require("mongoose");
+const { Schema } = require('mongoose');
 
 const UserSchema = new Schema(
     {
-        id: {
+        userId: {
             type: String,
             required: true,
+            unique: true,
         },
         password: {
             type: String,
@@ -13,15 +14,16 @@ const UserSchema = new Schema(
         email: {
             type: String,
             required: true,
+            unique: true,
         },
-        name: {
+        userName: {
             type: String,
             required: true,
         },
         role: {
             type: String,
-            enum: ["user", "admin"],
-            default: "user",
+            enum: ['user', 'admin'],
+            default: 'user',
         },
     },
     {
