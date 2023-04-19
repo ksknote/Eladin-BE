adaaba665782850d80aea158fc83814a7e606e43
heesankim <opqryt45@gmail.com>
2023-04-19 22:39:01 +0900

    Fix : module export 객체리터럴 형태로 변환

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const UserSchema = require("./models/schemas/userSchema");
-const ProductSchema = require("./models/schemas/productSchema");
-const orderSchema = require("./models/schemas/orderSchema");
+const UserSchema = require('./models/schemas/userSchema');
+const ProductSchema = require('./models/schemas/productSchema');
+const orderSchema = require('./models/schemas/orderSchema');
 
-exports.User = mongoose.model("User", UserSchema);
-exports.Product = mongoose.model("Product", ProductSchema);
-exports.Order = mongoose.model("Order", orderSchema);
+const User = mongoose.model('User', UserSchema);
+const Product = mongoose.model('Product', ProductSchema);
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = {
+    User,
+    Product,
+    Order,
+};
