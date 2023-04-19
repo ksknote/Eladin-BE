2358e35d8c54453979dab62f4108c3df216084a8
heesankim <opqryt45@gmail.com>
2023-04-19 22:41:34 +0900

    -Fix : orderItemSchema 위로 위치 옮김

--- src/db/models/schemas/orderSchema.js
+++ src/db/models/schemas/orderSchema.js
@@ -1,6 +1,23 @@
 const { Schema } = require('mongoose');
 // pdf >> 주문 번호, 배송지 정보(수령자, 핸드폰번호, 주소, 우편번호), 배송 방법, 배송 메모 등
 
+// [주문 목록 페이지] 각 주문 정보 >> 주문한 책, 주문 수량, 책 가격
+const orderItemSchema = new Schema({
+    product: {
+        type: String,
+        ref: 'Product',
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+});
+
 // [주문 완료 페이지] 총 주문 정보 >> 주문한 사용자, 주문한 책들, 주문 총액, 우편번호, 배송지 주소, 핸드폰 번호
 const orderSchema = new Schema(
     {
@@ -31,22 +48,5 @@ const orderSchema = new Schema(
         timestamps: true,
     }
 );
-
-// [주문 목록 페이지] 각 주문 정보 >> 주문한 책, 주문 수량, 책 가격
-const orderItemSchema = new Schema({
-    product: {
-        type: String,
-        ref: 'Product',
-        required: true,
-    },
-    quantity: {
-        type: Number,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-});
 
 module.exports = orderSchema;
