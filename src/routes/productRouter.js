9f9e340e77c85898a962fc9eef51d7b4b3b20575
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-21 03:38:10 +0900

    Fix: 각 라우터 주석 수정 in productRouter.js

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,16 +3,21 @@ const router = Router();
 
 const productService = require('../services/productService');
 
-router.get('/', productService.getAllProducts); // 책 전체 조회
+// [사용자] 책 목록 - 전체 책 조회
+router.get('/', productService.getAllProducts); // (메인 페이지)
 
-router.get('/:productId', productService.getProduct); // 책 단일 조회
+// (카테고리별 책 조회 추가 ?)
 
-router.post('/', productService.createProduct); // [관리자 전용] 책 추가하기
+// [사용자] 책 정보 상세 - 선택한 책 상세 정보 조회
+router.get('/:productId', productService.getProduct); // (책 상세 페이지)
 
-router.put('/:productId', productService.updateProduct); // [관리자 전용] 책 정보 수정
+// [관리자] 책 정보 추가
+router.post('/', productService.createProduct); // (관리 페이지)
 
-router.delete('/:productId', productService.deleteProduct); // [관리자 전용] 책 정보 삭제
+// [관리자] 책 정보 수정
+router.put('/:productId', productService.updateProduct); // (관리 페이지)
+
+// [관리자] 책 삭제
+router.delete('/:productId', productService.deleteProduct); // (관리 페이지)
 
 module.exports = router;
-
-//postman 으로 입력했을 때 데이터 베이스로 들어오는지
