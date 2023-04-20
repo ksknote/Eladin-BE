5a9b9d569a3bc9131cf84b7408bdd77888823e30
heesankim <opqryt45@gmail.com>
2023-04-20 19:39:51 +0900

    -Fix : 에러처리 부분 수정

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,79 +1,84 @@
 const { Product } = require('../db/index');
-
+const { AppError } = require('../middlewares/errorHandler');
 // 관리자
 const productService = {
     // 책 전체 조회
-    async getALLProducts(req, res) {
+    async getALLProducts(req, res, next) {
         try {
             const findALLProducts = await Product.find({});
-
-            res.status(201).json(findALLProducts);
+            res.status(200).json({ message: '모든 책 조회 성공', data: findALLProducts });
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: '책 조회 실패' });
+            console.error(error);
+            next(new AppError(500, '모든 책 조회 실패'));
         }
     },
 
     // 특정 책 조회
-    async getProduct(req, res) {
-        const { productId } = req.params;
-
-        try {
-            const foundProduct = await Product.findOne({ productId });
-
-            res.status(201).json(foundProduct);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: '단일 책 조회 실패' });
-        }
-    },
-
-    // [관리자 전용] 책 추가하기
-    async addProduct(req, res) {
-        const { productId, title, author, price, category, introduction } = req.body;
-        const createInfo = { productId, title, author, price, category, introduction };
-
-        try {
-            await Product.create(createInfo);
-
-            res.status(201).json({ message: '책 추가 성공' });
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: '책 추가 실패' });
-        }
-    },
-
-    // [관리자 전용] 책 정보 수정
-    async updateProduct(req, res) {
-        const { productId } = req.params;
-        const { title, author, price, category, introduction } = req.body;
-        const updateInfo = { title, author, price, category, introduction };
-
-        try {
-            const updatedProduct = await Product.updateOne({ productId }, updateInfo);
-
-            res.status(201).json(updatedProduct);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: '책 정보 수정 실패' });
-        }
-    },
-
-    // [관리자 전용] 책 정보 삭제
-    async deleteProduct(req, res) {
+    async getProduct(req, res, next) {
         const { productId } = req.params;
 
         try {
             const foundProduct = await Product.findOne({ productId });
             if (!foundProduct) {
-                throw new Error('삭제하실 책의 정보가 없습니다.');
+                return next(new AppError(404, '책을 찾을 수 없습니다.'));
+            }
+            res.status(200).json({ message: '단일 책 조회 성공 ', data: foundProduct });
+        } catch (error) {
+            console.error(error);
+            next(new AppError(500, '단일 책 조회 실패'));
+        }
+    },
+
+    // [관리자 전용] 책 추가하기
+    // [관리자 전용] 책 추가하기
+    async addProduct(req, res, next) {
+        const { productId, title, author, price, category, introduction } = req.body;
+        const createInfo = { productId, title, author, price, category, introduction };
+
+        try {
+            const addProduct = await Product.create(createInfo);
+            res.status(201).json({ message: '책 추가 성공', data: addProduct });
+        } catch (error) {
+            console.error(error);
+            next(new AppError(500, '책 추가 실패'));
+        }
+    },
+
+    // [관리자 전용] 책 정보 수정
+    async updateProduct(req, res, next) {
+        const { productId } = req.params;
+        const { title, author, price, category, introduction } = req.body;
+        const updateInfo = { title, author, price, category, introduction };
+
+        try {
+            const updatedProduct = await Product.updateOne({ productId }, updateInfo, {
+                new: true,
+            });
+            if (updatedProduct.n === 0) {
+                return next(new AppError(404, '책을 찾을 수 없습니다.'));
+            }
+            res.status(201).json({ message: '책 정보 수정 성공', data: updatedProduct });
+        } catch (error) {
+            console.error(error);
+            next(new AppError(500, '책 정보 수정 실패'));
+        }
+    },
+
+    // [관리자 전용] 책 정보 삭제
+    async deleteProduct(req, res, next) {
+        const { productId } = req.params;
+
+        try {
+            const foundProduct = await Product.findOne({ productId });
+            if (!foundProduct) {
+                return next(new AppError(404, '책을 찾을 수 없습니다.'));
             }
             await Product.deleteOne({ productId });
 
             res.status(201).json({ message: '책 정보 삭제 성공' });
         } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: '책 정보 삭제 실패' });
+            console.error(error);
+            next(new AppError(500, '책 정보 삭제 실패'));
         }
     },
 };
