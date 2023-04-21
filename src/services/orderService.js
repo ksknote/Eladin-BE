029c8fdee3c9fc6b7c4ef2359876c6e9827f8bce
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-21 20:58:17 +0900

    Update : 주문 라우터 기능 수정 in orderService.js

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -43,14 +43,13 @@ const orderService = {
     },
 
     // [관리자 전용] 배송상태 수정
-    // root : 관리자 전체 주문내역 페이지
     async updateDeliveryStatus(req, res) {
         try {
             const { orderNumber } = req.params;
             const { deliveryStatus } = req.body;
 
-            const updatedOrder = await Order.findByIdAndUpdate(
-                orderNumber,
+            const updatedOrder = await Order.updateOne(
+                { 'orderInfo.orderNumber': orderNumber },
                 { deliveryStatus },
                 { new: true }
             );
@@ -62,22 +61,21 @@ const orderService = {
         }
     },
 
-    // [사용자 전용] 주문 정보 수정 - 배송 시작 전까지 주문내역, 배송지정보 수정
-    // root : 사용자 주문 내역 페이지
+    // [사용자] 주문 정보 수정 - 배송 시작 전까지 주문 내역, 배송지정보 수정
     async updateDeliveryInfo(req, res) {
         try {
-            const { orderNumber } = req.params.orderInfo;
+            const { orderNumber } = req.params;
             const { items, deliveryInfo, deliveryStatus } = req.body;
 
             const newOrderNumber = Math.floor(Math.random() * 900000) + 100000;
             const newTotalPrice = items.reduce((acc, product) => acc + product.price, 0);
 
-            if (deliveryStatus === 'preparing') {
-                const updatedOrder = await Order.findByIdAndUpdate(
-                    { orderNumber },
+            if (deliveryStatus === 'pending') {
+                const updatedOrder = await Order.updateOne(
+                    { 'orderInfo.orderNumber': orderNumber },
                     {
-                        items, // 주문한 책 정보
-                        deliveryInfo,
+                        items, // 주문한 책 정보 수정
+                        deliveryInfo, // 배송지 정보 수정
                         orderInfo: {
                             orderNumber: newOrderNumber,
                             totalPrice: newTotalPrice,
@@ -94,12 +92,15 @@ const orderService = {
         }
     },
 
-    // [사용자 전용] 주문 완료 - 현재 주문내역 조회
+    // [사용자] 주문 완료 - 현재 주문내역 조회
     async getCurrentOrder(req, res) {
         try {
-            const { orderNumber } = req.params.orderInfo;
+            const { orderNumber } = req.params;
 
-            const foundOrder = await Order.findOne({ orderNumber }, { items: 0 }); // 책 정보 제외
+            const foundOrder = await Order.findOne(
+                { 'orderInfo.orderNumber': orderNumber },
+                { items: 0 } // 책 정보 제외
+            );
 
             res.status(201).json({ message: '현재 주문내역 조회 성공', data: foundOrder });
         } catch (error) {
@@ -108,7 +109,7 @@ const orderService = {
         }
     },
 
-    // [사용자 전용] 주문 조회 - 전체 주문내역 조회
+    // [사용자] 주문 조회 - 개인 주문내역 조회
     async getOrdersByUserId(req, res) {
         try {
             const { userId } = req.params;
@@ -122,7 +123,7 @@ const orderService = {
         }
     },
 
-    // [관리자 전용] 주문 조회 - 전체 이용자 주문내역 조회
+    // [관리자] 주문 조회 - 전체 주문내역 조회
     async getAllOrders(req, res) {
         try {
             const foundAllOrders = await Order.find({});
@@ -137,16 +138,19 @@ const orderService = {
         }
     },
 
-    // [사용자 전용] 주문 취소
+    // [사용자] 주문 취소 - 사용자 주문 취소
     async cancelOrder(req, res) {
         try {
-            const { orderNumber } = req.params.orderInfo;
+            const { orderNumber } = req.params;
 
-            const foundOrder = await Order.find({ orderNumber });
+            const foundOrder = await Order.findOne({ 'orderInfo.orderNumber': orderNumber });
+
             if (foundOrder.deliveryStatus === 'pending') {
-                await foundOrder.deleteOne({ orderNumber });
+                const canceledOrder = await foundOrder.deleteOne({
+                    'orderInfo.orderNumber': orderNumber,
+                });
 
-                res.status(201).json({ message: '주문 취소 성공' });
+                res.status(201).json({ message: '주문 취소 성공', data: canceledOrder });
             }
         } catch (error) {
             console.log(error);
@@ -154,14 +158,14 @@ const orderService = {
         }
     },
 
-    // [관리자 전용] 주문 삭제
+    // [관리자] 주문 삭제 - 관리자 주문 삭제
     async deleteOrder(req, res) {
         try {
-            const { orderNumber } = req.params.orderInfo;
+            const { orderNumber } = req.params;
 
-            await Order.deleteOne({ orderNumber });
+            const deletedOrder = await Order.deleteOne({ 'orderInfo.orderNumber': orderNumber });
 
-            res.status(201).json({ message: '주문 삭제 성공' });
+            res.status(201).json({ message: '주문 삭제 성공', data: deletedOrder });
         } catch (error) {
             console.log(error);
             res.status(500).json({ message: '주문 삭제 실패' });
