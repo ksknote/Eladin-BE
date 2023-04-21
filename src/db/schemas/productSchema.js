06363496344344c04773c90d0a150481c4df625b
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-21 23:33:54 +0900

    Update : add 'publisher' field in productSchema.js

--- src/db/schemas/productSchema.js
+++ src/db/schemas/productSchema.js
@@ -26,6 +26,10 @@ const ProductSchema = new Schema({
         type: String,
         required: true,
     },
+    publisher: {
+        type: String,
+        required: true,
+    },
 });
 
 module.exports = ProductSchema;
