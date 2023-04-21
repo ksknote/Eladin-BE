a6ad5ebd0b5c8bbe1034bf61fd099d4a0a5c5690
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-21 20:54:33 +0900

    Update : 주문라우터 파라미터 key 수정 in orderRouter.js

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -7,24 +7,24 @@ const orderService = require('../services/orderService');
 router.post('/', orderService.createOrder); // (주문 페이지)
 
 // [관리자] 배송상태 수정
-router.patch('/:orderInfo/delivery-status', orderService.updateDeliveryStatus); // (관리 페이지)
+router.patch('/:orderNumber/delivery-status', orderService.updateDeliveryStatus); // (관리 페이지)
 
-// [사용자] 주문정보 수정 - 배송 시작 전까지 주문내역, 배송지정보 수정
-router.patch('/:orderInfo/delivery-info', orderService.updateDeliveryInfo);
+// [사용자] 주문정보 수정 - 배송 시작 전까지 주문 내역, 배송지정보 수정
+router.patch('/:orderNumber/delivery-info', orderService.updateDeliveryInfo); // (주문내역 수정 페이지)
 
 // [사용자] 주문 완료 - 현재 주문내역 조회
-router.get('/:orderInfo/complete-info', orderService.getCurrentOrder); // (주문완료 페이지)
+router.get('/:orderNumber/current-order', orderService.getCurrentOrder); // (주문완료 페이지)
 
-// [사용자] 주문 조회 - 전체 주문내역 조회
+// [사용자] 주문 조회 - 개인 주문내역 조회
 router.get('/:userId/user', orderService.getOrdersByUserId); // (주문내역 페이지)
 
-// [관리자] 주문 조회 - 전체 이용자 주문내역 조회
+// [관리자] 주문 조회 - 전체 주문내역 조회
 router.get('/', orderService.getAllOrders); // (관리 페이지)
 
-// [사용자] 주문 취소 - 주문번호로 주문 취소
-router.delete('/:orderInfo/user-cancel', orderService.cancelOrder); // (주문내역 페이지)
+// [사용자] 주문 취소 - 사용자 주문 취소
+router.delete('/:orderNumber/user-cancel', orderService.cancelOrder); // (주문내역 페이지)
 
-// [관리자] 주문 삭제 - 주문번호로 주문 삭제
-router.delete('/:orderInfo/admin-delete', orderService.deleteOrder); // (관리 페이지)
+// [관리자] 주문 삭제 - 관리자 주문 삭제
+router.delete('/:orderNumber/admin-delete', orderService.deleteOrder); // (관리 페이지)
 
 module.exports = router;
