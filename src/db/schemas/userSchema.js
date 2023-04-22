fecfacb1a757202a485c0478b9641c5417e879ba
heesankim <opqryt45@gmail.com>
2023-04-22 20:46:14 +0900

    feat : add 이메일형식 유효성 검사 in userSchema.js

--- src/db/schemas/userSchema.js
+++ src/db/schemas/userSchema.js
@@ -18,6 +18,7 @@ const UserSchema = new Schema(
             type: String,
             required: true,
             unique: true,
+            match: /.+@.+..+/,
         },
         userName: {
             type: String,
