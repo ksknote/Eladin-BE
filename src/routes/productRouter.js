222bae2c327daa440a524f57997d42bf338d0d56
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-23 19:27:39 +0900

    Feat : 카테고리 추가, 삭제 라우터 추가 in productRouter.js

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -8,14 +8,16 @@ const productService = require('../services/productService');
 // [사용자] 카테고리 조회 - 카테고리 목록 조회
 router.get('/categories', productService.getCategoryList); // (카테고리 목록 페이지)
 
-// [관리자] 카테고리 추가 - 카테고리 추가 >>> 그냥 입력한 카테고리만 넣고 나머지 필드는 없이 만들자
-// router.post('admin/category');
+// [관리자] 카테고리 추가 - 카테고리 추가
+// 그냥 입력한 카테고리만 넣고 나머지 필드는 없이 만들고, 나머지 라우터에서 조회할때는 필드 있는거만 조회되게 하자
+router.post('/admin/category', productService.createCategory);
 
 // [관리자] 카테고리 수정 - 카테고리 수정 (해당하는 모든 책에 반영)
 router.patch('/admin/category', productService.updateCategory); // (카테고리 관리 페이지)
 
-// [관리자] 카테고리 삭제 - 카테고리 삭제 >>> 삭제되면 카테고리에 해당하는 책까지 다 삭제 해야함
-// router.delete('admin/category');
+// [관리자] 카테고리 삭제 - 카테고리 삭제
+// 삭제되면 카테고리에 해당하는 책까지 다 삭제 해야함
+router.delete('/admin/category', productService.deleteCategory);
 
 // [관리자] 상품 추가 - 책 정보 추가 (productId는 서버에서 새로 생성함)
 router.post('/admin', productService.createProduct); // (상품 관리 페이지)
