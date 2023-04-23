a79acdf1b93889e787517ca3a731e2e6cd0cddb9
heesankim <opqryt45@gmail.com>
2023-04-23 20:32:48 +0900

    Feat : origin 주소 추가

--- src/app.js
+++ src/app.js
@@ -12,8 +12,14 @@ const authRouter = require('./routes/authRouter');
 const orderRouter = require('./routes/orderRouter');
 const productRouter = require('./routes/productRouter');
 const port = Number(env.PORT || 3000);
+const allowedOrigins = ['http://localhost:5501', 'http://localhost:5500'];
+app.use(
+    cors({
+        origin: allowedOrigins,
+        credentials: true, // 쿠키를 허용하기 위한 설정
+    })
+);
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
