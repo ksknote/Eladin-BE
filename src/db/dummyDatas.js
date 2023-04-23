0248b34a1b0216ecb533f57fd0d2e22b0f154ac2
heesankim <opqryt45@gmail.com>
2023-04-23 16:01:36 +0900

    - fix : 더미데이터 오류 수정

--- src/db/dummyDatas.js
+++ src/db/dummyDatas.js
@@ -4225,6 +4225,73 @@ const dummyProducts = [
     },
 ];
 
+const insertDummyProducts = async () => {
+    try {
+        await Product.deleteMany({}, { maxTimeMS: 60000 });
+        await Product.create(dummyProducts);
+        console.log('Dummy products inserted successfully');
+    } catch (error) {
+        console.log('Error inserting dummy products: ', error);
+    }
+};
+const dummyUsers = [
+    {
+        userId: 'user1',
+        password: bcrypt.hashSync('password1', 10),
+        email: 'user1@example.com',
+        userName: 'John Doe',
+        role: 'user',
+        tokens: {
+            accessToken: 'dummy_access_token_1',
+            refreshToken: 'dummy_refresh_token_1',
+        },
+    },
+    {
+        userId: 'admin1',
+        password: bcrypt.hashSync('password1', 10),
+        email: 'admin1@example.com',
+        userName: 'Jane Doe',
+        role: 'admin',
+        tokens: {
+            accessToken: 'dummy_access_token_2',
+            refreshToken: 'dummy_refresh_token_2',
+        },
+    },
+    {
+        userId: 'user2',
+        password: bcrypt.hashSync('password2', 10),
+        email: 'user2@example.com',
+        userName: 'Alice',
+        role: 'user',
+        tokens: {
+            accessToken: 'dummy_access_token_3',
+            refreshToken: 'dummy_refresh_token_3',
+        },
+    },
+    {
+        userId: 'admin2',
+        password: bcrypt.hashSync('password2', 10),
+        email: 'admin2@example.com',
+        userName: 'Bob',
+        role: 'admin',
+        tokens: {
+            accessToken: 'dummy_access_token_4',
+            refreshToken: 'dummy_refresh_token_4',
+        },
+    },
+    {
+        userId: 'user3',
+        password: bcrypt.hashSync('password3', 10),
+        email: 'user3@example.com',
+        userName: 'Eve',
+        role: 'user',
+        tokens: {
+            accessToken: 'dummy_access_token_5',
+            refreshToken: 'dummy_refresh_token_5',
+        },
+    },
+];
+
 const insertDummyUsers = async () => {
     try {
         await User.deleteMany({});
