b49499be5cc88f4509f0f24168c0a0904354f629
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-24 19:10:28 +0900

    Omment : 주석 추가 in productRouter.js

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,10 +3,8 @@ const router = Router();
 
 const productService = require('../services/productService');
 
-// 필드 추가하기
-
 // [사용자] 카테고리 조회 - 카테고리 목록 조회
-router.get('/categories', productService.getCategoryList); // (카테고리 목록 페이지)
+router.get('/categories', productService.getCategoryList); // (카테고리 목록 페이지), (상품 관리 페이지)
 
 // [관리자] 카테고리 추가 - 카테고리 추가
 // 그냥 입력한 카테고리만 넣고 나머지 필드는 없이 만들고, 나머지 라우터에서 조회할때는 필드 있는거만 조회되게 하자
@@ -19,7 +17,9 @@ router.patch('/admin/category', productService.updateCategory); // (카테고리
 // 삭제되면 카테고리에 해당하는 책까지 다 삭제 해야함
 router.delete('/admin/category', productService.deleteCategory);
 
-// [관리자] 상품 추가 - 책 정보 추가 (productId는 서버에서 새로 생성함)
+// [관리자] 상품 추가 - 책 정보 추가
+// productId는 서버에서 새로 생성함
+// category는 카테고리 조회 라우터에서 카테고리 리스트 받아서 사용자 선택한 값 보내야함
 router.post('/admin', productService.createProduct); // (상품 관리 페이지)
 
 // [관리자] 상품 수정 - 책 정보 수정
@@ -35,6 +35,9 @@ router.get('/', productService.getAllProducts); // (메인 페이지)
 // [사용자] 상품 목록 - 카테고리별 책 목록 조회
 // 카테고리 이름만 있는 데이터는 조회 안되게 추가하기
 router.get('/categories/:category', productService.getProductByCategory); // (카테고리 관리 페이지)
+
+// [사용자] 상품 목록 - 신간, 추천, 베스트 책 목록 조회
+// router.get('/d')
 
 // [사용자] 상품 상세 - 선택한 책의 상세정보 조회
 router.get('/:productId', productService.getProductByProductId); // (책 상세 페이지)
