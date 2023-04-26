639267c93faf795ba02b7a5ba88b50efd06c82d0
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-26 12:21:28 +0900

    Refac : 상품 주문 API 리팩토링 in orderService.js

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,25 +1,29 @@
 const { Order } = require('../db/models/index');
+const { Product } = require('../db/models/index');
+const { User } = require('../db/models/index');
+const { index } = require('../db/schemas/userSchema');
+const { AppError } = require('../middlewares/errorHandler');
+const { ObjectId } = require('mongoose');
 
 const orderService = {
-    // 임의 6자리 주문번호
-    getOrderNumber() {
-        return Math.floor(Math.random() * 900000) + 100000;
-    },
-
-    // 주문 총액
-    getTotalPrice(items) {
-        return items.reduce((acc, product) => acc + product.price);
-    },
-
-    // [사용자 전용] 주문 추가 - 추가할 때 마다 새로운 주문번호 생성
-    async createOrder(req, res) {
+    // [사용자] 주문 추가 - 추가할 때 마다 새로운 주문번호 생성
+    async createOrder(req, res, next) {
         try {
             const { userId, items, deliveryInfo } = req.body;
 
-            // const orderNumber = this.getOrderNumber();
-            // const totalPrice = this.getTotalPrice(items);
-            const orderNumber = Math.floor(Math.random() * 900000) + 100000;
-            const totalPrice = items.reduce((acc, product) => acc + product.price, 0);
+            if (!userId || !items || !deliveryInfo)
+                return next(new AppError(400, '주문정보를 모두 입력해 주세요.'));
+
+            const foundUser = await User.findOne({ userId });
+            if (!foundUser) return next(new AppError(400, '존재하지 않는 사용자 입니다'));
+
+            const orderNumber =
+                Date.now().toString().slice(-9) +
+                Math.floor(Math.random() * 1000)
+                    .toString()
+                    .padStart(3, '0');
+            //Math.floor(Math.random() * 900000) + 100000;
+            const totalPrice = items.reduce((acc, item) => acc + item.quantity * item.price, 0);
 
             const createInfo = {
                 userId,
@@ -33,20 +37,64 @@ const orderService = {
 
             const createdOrder = await Order.create(createInfo);
 
-            console.log('주문 추가 내용 : ', createdOrder);
-
             res.status(201).json({ message: '주문 추가 성공', data: createdOrder });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[주문 등록] 서버 에러'));
+        }
+    },
+
+    // [비회원] 주문 등록 - 추가할 때 마다 새로운 주문번호 생성
+    async createOrderForNonMember(req, res, next) {
+        try {
+            const { uuid, items, deliveryInfo } = req.body;
+
+            if (!uuid || !items || !deliveryInfo)
+                return next(new AppError(400, '주문정보를 모두 입력해 주세요.'));
+
+            const foundUser = await User.findOne({ uuid });
+            if (!foundUser) return next(new AppError(400, '존재하지 않는 비회원 사용자 입니다'));
+
+            const orderNumber =
+                Date.now().toString().slice(-9) +
+                Math.floor(Math.random() * 1000)
+                    .toString()
+                    .padStart(3, '0');
+            //Math.floor(Math.random() * 900000) + 100000;
+            const totalPrice = items.reduce((acc, item) => acc + item.quantity * item.price, 0);
+
+            const createInfo = {
+                uuid,
+                items,
+                deliveryInfo,
+                orderInfo: {
+                    orderNumber,
+                    totalPrice,
+                },
+            };
+
+            const createdOrder = await Order.create(createInfo);
+
+            res.status(201).json({
+                message: '비회원 주문 추가 성공',
+                data: { createdOrder, userName: foundUser.userName },
+            });
+        } catch (error) {
+            console.log(error);
+            next(new AppError(500, '[비회원 주문 등록] 서버 에러'));
         }
     },
 
     // [관리자] 주문 수정 - 배송상태 수정
-    async updateDeliveryStatus(req, res) {
+    async updateDeliveryStatus(req, res, next) {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message: '접근 권한이 없습니다.' });
+        }
         try {
-            const { orderNumber } = req.params;
-            const { deliveryStatus } = req.body;
+            const { orderNumber, deliveryStatus } = req.body;
+
+            if (!orderNumber || !deliveryStatus)
+                return next(new AppError(400, '주문번호와 배송 상태 정보를 모두 입력해 주세요.'));
 
             const updatedOrder = await Order.updateOne(
                 { 'orderInfo.orderNumber': orderNumber },
@@ -54,23 +102,31 @@ const orderService = {
                 { new: true }
             );
 
-            res.status(200).json({ message: '배송상태 수정 성공', data: updatedOrder });
+            const foundUpdatedOrder = await Order.findOne({ 'orderInfo.orderNumber': orderNumber });
+
+            res.status(200).json({ message: '배송상태 수정 성공', data: foundUpdatedOrder });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[관리자 주문 수정] 서버 에러'));
         }
     },
 
     // [사용자] 주문 수정 - 배송 시작 전까지 주문내역, 배송지정보 수정
-    async updateDeliveryInfo(req, res) {
+    async updateDeliveryInfo(req, res, next) {
         try {
-            const { orderNumber } = req.params;
-            const { items, deliveryInfo, deliveryStatus } = req.body;
+            const { orderNumber, items, deliveryInfo, deliveryStatus } = req.body;
 
-            const newOrderNumber = Math.floor(Math.random() * 900000) + 100000;
-            const newTotalPrice = items.reduce((acc, product) => acc + product.price, 0);
+            if (!orderNumber || !items || !deliveryInfo || !deliveryStatus)
+                return next(new AppError(400, '수정 정보를 모두 입력해 주세요.'));
 
-            if (deliveryStatus === 'pending') {
+            const newOrderNumber =
+                Date.now().toString().slice(-9) +
+                Math.floor(Math.random() * 1000)
+                    .toString()
+                    .padStart(3, '0');
+            const newTotalPrice = items.reduce((acc, item) => acc + item.quantity * item.price, 0);
+
+            if (deliveryStatus === '배송 준비 중' || deliveryStatus === '배송 완료^^') {
                 const updatedOrder = await Order.updateOne(
                     { 'orderInfo.orderNumber': orderNumber },
                     {
@@ -84,95 +140,225 @@ const orderService = {
                     { new: true }
                 );
 
-                res.status(200).json({ message: '주문정보 수정 성공', data: updatedOrder });
+                const foundUpdatedOrder = await Order.findOne({
+                    'orderInfo.orderNumber': newOrderNumber,
+                });
+
+                if (!foundUpdatedOrder)
+                    return next(
+                        new AppError(
+                            400,
+                            '주문번호가 변경되었거나, 관리자에 의해 삭제된 주문입니다.'
+                        )
+                    );
+
+                res.status(200).json({ message: '주문정보 수정 성공', data: foundUpdatedOrder });
             }
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[사용자 주문 수정] 서버 에러'));
         }
     },
 
     // [사용자] 주문 완료 - 현재 주문내역 조회
-    async getCurrentOrder(req, res) {
+    async getCurrentOrder(req, res, next) {
         try {
-            const { orderNumber } = req.params;
+            const { orderNumber } = req.body;
+
+            if (!orderNumber) return next(new AppError(400, '주문번호를 입력해 주세요.'));
 
             const foundOrder = await Order.findOne(
-                { 'orderInfo.orderNumber': orderNumber },
-                { items: 0 } // 책 정보 제외
+                { 'orderInfo.orderNumber': orderNumber }
+                // { items: 0 } // 책 정보 제외
             );
 
-            res.status(200).json({ message: '현재 주문내역 조회 성공', data: foundOrder });
+            if (!foundOrder) return next(new AppError(400, '현재 주문내역이 존재하지 않습니다.'));
+
+            const productIds = foundOrder.items.map((item) => item.productId);
+            const products = await Product.find({ productId: { $in: productIds } }, { title: 1 });
+            const titles = products.map((product) => product.title);
+
+            res.status(200).json({
+                message: '현재 주문내역 조회 성공',
+                data: { foundOrder, titles },
+            });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[현재 주문내역 조회] 서버 에러'));
         }
     },
 
     // [사용자] 주문 조회 - 개인 주문내역 조회
-    async getOrdersByUserId(req, res) {
+    async getMyAllOrders(req, res, next) {
         try {
-            const { userId } = req.params;
+            const { userId } = req.body;
+
+            if (!userId) return next(new AppError(400, 'userId를 입력해 주세요'));
 
             const foundOrders = await Order.find({ userId });
 
-            res.status(200).json({ message: '사용자 전체 주문내역 조회 성공', data: foundOrders });
+            if (!foundOrders) return next(new AppError(400, '개인 주문내역이 존재하지 않습니다.'));
+
+            const titleList = [],
+                imgUrlList = [];
+
+            for (let order of foundOrders) {
+                const productIds = order.items.map((item) => item.productId);
+                const products = await Product.find(
+                    { productId: { $in: productIds } },
+                    { title: 1, imgUrl: 1 }
+                );
+                const titles = products.map((product) => product.title);
+                const imgUrls = products.map((product) => product.imgUrl);
+
+                titleList.push(titles);
+                imgUrlList.push(imgUrls);
+            }
+
+            res.status(200).json({
+                message: '개인 주문내역 조회 성공',
+                data: { foundOrders, titleList, imgUrlList },
+            });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[개인 주문내역 조회] 서버 에러'));
+        }
+    },
+
+    // [비회원] 주문 조회 - 개인 주문내역 조회
+    async getMyAllOrdersForNonMember(req, res, next) {
+        try {
+            const { uuid } = req.body;
+
+            if (!uuid) return next(new AppError(400, 'uuid를 입력해 주세요'));
+
+            const foundOrders = await Order.find({ uuid });
+
+            if (!foundOrders)
+                return next(new AppError(400, '비회원 개인 주문내역이 존재하지 않습니다.'));
+
+            const titleList = [],
+                imgUrlList = [];
+
+            for (let order of foundOrders) {
+                const productIds = order.items.map((item) => item.productId);
+                const products = await Product.find(
+                    { productId: { $in: productIds } },
+                    { title: 1, imgUrl: 1 }
+                );
+                const titles = products.map((product) => product.title);
+                const imgUrls = products.map((product) => product.imgUrl);
+
+                titleList.push(titles);
+                imgUrlList.push(imgUrls);
+            }
+
+            res.status(200).json({
+                message: '비회원 개인 주문내역 조회 성공',
+                data: { foundOrders, titleList, imgUrlList },
+            });
+        } catch (error) {
+            console.log(error);
+            next(new AppError(500, '[개인 주문내역 조회] 서버 에러'));
         }
     },
 
     // [관리자] 주문 조회 - 전체 주문내역 조회
-    async getAllOrders(req, res) {
+    async getAllOrders(req, res, next) {
         if (req.user.role !== 'admin') {
             return res.status(403).json({ message: '접근 권한이 없습니다.' });
         }
-
         try {
             const foundAllOrders = await Order.find({});
 
+            if (!foundAllOrders)
+                return next(new AppError(400, '전체 주문내역이 존재하지 않습니다.'));
+
+            const titleList = [];
+            for (let order of foundAllOrders) {
+                const productIds = order.items.map((item) => item.productId);
+                const products = await Product.find(
+                    { productId: { $in: productIds } },
+                    { title: 1 }
+                );
+                const titles = products.map((product) => product.title);
+
+                titleList.push(titles);
+            }
+
+            const userNameList = [];
+
+            for (let order of foundAllOrders) {
+                const foundUserName = await User.findOne({ userId: order.userId });
+                userNameList.push(foundUserName.userName);
+            }
+
             res.status(200).json({
                 message: '관리자 전체 주문내역 조회 성공',
-                data: foundAllOrders,
+                data: { foundAllOrders, titleList, userNameList },
             });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[전체 주문내역 조회] 서버 에러'));
         }
     },
 
     // [사용자] 주문 취소 - 사용자 주문 취소
-    async cancelOrder(req, res) {
+    async cancelOrder(req, res, next) {
         try {
-            const { orderNumber } = req.params;
+            const { orderNumber } = req.body;
+
+            if (!orderNumber) return next(new AppError(400, '주문번호를 입력해 주세요.'));
 
             const foundOrder = await Order.findOne({ 'orderInfo.orderNumber': orderNumber });
 
-            if (foundOrder.deliveryStatus === 'pending') {
-                const canceledOrder = await foundOrder.deleteOne({
-                    'orderInfo.orderNumber': orderNumber,
-                });
+            if (!foundOrder)
+                return next(
+                    new AppError(400, '주문번호가 변경되었거나, 관리자에 의해 삭제된 주문입니다.')
+                );
 
-                res.status(200).json({ message: '주문 취소 성공', data: canceledOrder });
-            }
+            if (
+                foundOrder.deliveryStatus !== '배송 준비 중' ||
+                foundOrder.deliveryStatus === '배송 완료^^'
+            )
+                return next(new AppError(400, '배송이 시작되어 취소하실 수 없습니다.'));
+
+            const canceledOrder = await foundOrder.deleteOne({
+                'orderInfo.orderNumber': orderNumber,
+            });
+
+            res.status(200).json({ message: '주문 취소 성공', data: canceledOrder });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[사용자 주문 취소]서버 에러'));
         }
     },
 
     // [관리자] 주문 삭제 - 관리자 주문 삭제
-    async deleteOrder(req, res) {
+    async deleteOrder(req, res, next) {
+        if (req.user.role !== 'admin') {
+            return res.status(403).json({ message: '접근 권한이 없습니다.' });
+        }
         try {
-            const { orderNumber } = req.params;
+            const { orderNumber } = req.body;
+
+            if (!orderNumber) return next(new AppError(400, '주문번호를 입력해 주세요.'));
+
+            const foundDeletedOrder = await Order.findOne({
+                'orderInfo.orderNumber': orderNumber,
+            });
+
+            if (!foundDeletedOrder)
+                return next(
+                    new AppError(400, '주문번호가 변경되었거나, 사용자가 이미 취소한 주문입니다.')
+                );
 
             const deletedOrder = await Order.deleteOne({ 'orderInfo.orderNumber': orderNumber });
 
-            res.status(200).json({ message: '주문 삭제 성공', data: deletedOrder });
+            res.status(200).json({ message: '주문 삭제 성공', data: foundDeletedOrder });
         } catch (error) {
             console.log(error);
-            res.status(500).json({ message: '서버 에러' });
+            next(new AppError(500, '[관리자 주문 삭제] 서버 에러'));
         }
     },
 };
