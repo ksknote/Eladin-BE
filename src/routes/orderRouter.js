94770f79cc73cdb72896458c996f1664462e003c
[SW4/2반] 박지원 <parkji12532@gmail.com>
2023-04-26 12:18:02 +0900

    Feat : jwt, uuid 유효성검사 미들웨어 추가 in orderRouter.js

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -3,32 +3,35 @@ const router = Router();
 
 const orderService = require('../services/orderService');
 const { isAccessTokenValid } = require('../middlewares/jwt');
-// [사용자] 주문 추가 ( 회원,비회원 카테고리 페이지 or 장바구니에서 바로구매 눌렀을 때 )
-router.post('/', orderService.createOrder); // (주문 페이지)
+
+// [사용자] 주문 등록 ( 회원,비회원 카테고리 페이지 or 장바구니에서 바로구매 눌렀을 때 )
+router.post('/user', isAccessTokenValid, orderService.createOrder); // (주문 페이지)
+
+// [비회원] 주문 등록
+router.post('/nonmember', isAccessTokenValid, orderService.createOrderForNonMember);
 
 // [관리자] 주문 수정 - 배송상태 수정
-router.patch(
-    '/:orderNumber/delivery-status',
-    isAccessTokenValid,
-    orderService.updateDeliveryStatus
-); // (관리 페이지)
+router.patch('/admin', isAccessTokenValid, orderService.updateDeliveryStatus); // (관리 페이지)
 
 // [사용자] 주문 수정 - 배송 시작 전까지 주문내역, 배송지정보 수정
-router.patch('/:orderNumber/delivery-info', isAccessTokenValid, orderService.updateDeliveryInfo); // (주문내역 수정 페이지)
+router.patch('/user', isAccessTokenValid, orderService.updateDeliveryInfo); // (주문내역 수정 페이지)
 
-// [사용자] 주문 완료 - 현재 주문내역 조회
-router.get('/:orderNumber/current-order', isAccessTokenValid, orderService.getCurrentOrder); // (주문완료 페이지)
+// [사용자/비회원] 주문 완료 - 현재 주문내역 조회
+router.get('/user/complete', isAccessTokenValid, orderService.getCurrentOrder); // (주문완료 페이지)
 
 // [사용자] 주문 조회 - 개인 주문내역 조회
-router.get('/:userId/user', isAccessTokenValid, orderService.getOrdersByUserId); // (주문내역 페이지)
+router.get('/user', isAccessTokenValid, orderService.getMyAllOrders); // (주문내역 페이지)
+
+// [비회원] 주문 조회 - 개인 주문내역 조회
+router.get('/nonmember', isAccessTokenValid, orderService.getMyAllOrdersForNonMember);
 
 // [관리자] 주문 조회 - 전체 주문내역 조회
-router.get('/', isAccessTokenValid, orderService.getAllOrders); // (관리 페이지)
+router.get('/admin', isAccessTokenValid, orderService.getAllOrders); // (관리 페이지)
 
 // [사용자] 주문 취소 - 사용자 주문 취소
-router.delete('/:orderNumber/user-cancel', isAccessTokenValid, orderService.cancelOrder); // (주문내역 페이지)
+router.delete('/user', isAccessTokenValid, orderService.cancelOrder); // (주문내역 페이지)
 
 // [관리자] 주문 삭제 - 관리자 주문 삭제
-router.delete('/:orderNumber/admin-delete', isAccessTokenValid, orderService.deleteOrder); // (관리 페이지)
+router.delete('/admin', isAccessTokenValid, orderService.deleteOrder); // (관리 페이지)
 
 module.exports = router;
